a955afc3693a9f0ab70ec80248059ce35b951eed
MLRichter <matrichter@uos.de>
2022-04-07 21:11:41 +0200

    new training script

--- imagenet_to_tfr.py
+++ imagenet_to_tfr.py
@@ -335,6 +335,7 @@ def convert_to_tf_records(
 
   validation_files = tf.gfile.Glob(
       os.path.join(raw_data_dir, VALIDATION_DIRECTORY, '*', '*.JPEG'))
+  logging.info("FILES:", validation_files)
 
   # Get training file synset labels from the directory name
   validation_synsets = [
@@ -345,7 +346,6 @@ def convert_to_tf_records(
   validation_files = [validation_files[i] for i in validation_shuffle_idx]
   validation_synsets = [validation_synsets[i] for i in validation_shuffle_idx]
 
-  logging.info(validation_files)
 
   # Glob all the training files
   training_files = tf.gfile.Glob(
