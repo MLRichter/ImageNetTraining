6a61b59fba517b022cf09ea62f4c207a3a7bfb81
MLRichter <matrichter@uos.de>
2022-05-14 23:50:32 +0200

    hb

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -946,6 +946,27 @@ def efficientnet_b5_perf5(*args, **kwargs):
 
 
 @register_model
+def efficientnet_b5_perf52(*args, **kwargs):
+    bneck_conf = partial(MBConvConfig, width_mult=1.6, depth_mult=2.2)
+    inverted_residual_setting = [
+        bneck_conf(1, 3, 1, 32, 16, 1),
+        bneck_conf(6, 3, 2, 16, 24, 2),
+        bneck_conf(6, 5, 2, 24, 40, 2),
+        bneck_conf(6, 3, 1, 40, 80, 3),
+        bneck_conf(6, 5, 2, 80, 112, 3),
+        bneck_conf(6, 5, 1, 112, 192, 4),
+        bneck_conf(6, 3, 1, 192, 320, 1),
+    ]
+    if "pretrained" in kwargs:
+        kwargs.pop("pretrained")
+    model = _efficientnet("efficientnet_b5", width_mult=1.6, depth_mult=2.2, dropout=0.3, pretrained=False,
+                          progress=True, inverted_residual_setting=inverted_residual_setting,
+                          **kwargs)
+    model.name = "EfficentNetB5_Performance52"
+    return model
+
+
+@register_model
 def efficientnet_b6_perf5(*args, **kwargs):
     bneck_conf = partial(MBConvConfig, width_mult=1.8, depth_mult=2.6)
     inverted_residual_setting = [
