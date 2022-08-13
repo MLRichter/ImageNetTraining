75fd8e22bc2c51324132ef19c12b9a8013ac83ab
Anna Wiedenroth <awiedenroth@uos.de>
2022-08-13 18:36:42 -0400

    next try on gradient accumula

--- train.py
+++ train.py
@@ -750,7 +750,7 @@ def train_one_epoch(
         if loss_scaler is not None:
             loss_scaler(
                 loss, optimizer,
-                clip_grad=args.clip_grad, clip_mode=args.clip_mode,
+                clip_grad=args.clip_grad,
                 parameters=model_parameters(model, exclude_head='agc' in args.clip_mode),
                 create_graph=second_order, update_grad=update_grad)
             if update_grad:
