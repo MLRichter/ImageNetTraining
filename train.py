a5b42705c622e4952db15511e84977289a26f907
MLRichter <matrichter@uos.de>
2023-01-26 13:03:08 -0500

    added a logging tool

--- train.py
+++ train.py
@@ -389,8 +389,8 @@ def main():
     if args.distributed:
         args.device = 'cuda:%d' % args.local_rank
         torch.cuda.set_device(args.local_rank)
-        ngpus_per_node = torch.cuda.device_count()
-        args.rank = int(os.environ.get("SLURM_NODEID"))*ngpus_per_node + args.local_rank
+        args.rank = int(os.environ.get("SLURM_PROCID"))
+        args.local_rank = int(os.environ.get("SLURM_LOCALID"))
         print("Initializing process", args.rank, "with local rank", args.local_rank)
         torch.distributed.init_process_group(world_size=args.world_size,
                                              backend='nccl',
