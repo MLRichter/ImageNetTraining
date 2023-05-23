2bc0d2905988d36183c68d6791916c707d7f0067
MLRichter <matrichter@uos.de>
2023-05-23 15:16:22 -0400

    efficientMobileNetV1

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -106,9 +106,65 @@ class MobileNetV1(nn.Module):
         x = self.fc(x)
         return x
 
+
+class EfficientMobileNetV1(nn.Module):
+
+    def __init__(self, ch_in=3, n_classes=1000, pretrained=False, **kwargs):
+        super(EfficientMobileNetV1, self).__init__()
+        self.default_cfg = {}
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+                )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                # dw
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+
+                # pw
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+                )
+
+        self.model = nn.Sequential(
+            conv_bn(ch_in, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            #conv_dw(512, 1024, 2),
+            #conv_dw(1024, 1024, 1),
+            nn.AdaptiveAvgPool2d(1)
+        )
+        self.fc = nn.Linear(512, n_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 512)
+        x = self.fc(x)
+        return x
+
 @register_model
 def mobilenetV1(*args, weights=None, progress: bool = True, **kwargs):
     return MobileNetV1(**kwargs)
+
+@register_model
+def efficient_mobilenetV1(*args, weights=None, progress: bool = True, **kwargs):
+    return EfficientMobileNetV1(**kwargs)
 
 @register_model
 def better_mobilenetV1(*args, weights=None, progress: bool = True, **kwargs):
@@ -117,9 +173,10 @@ def better_mobilenetV1(*args, weights=None, progress: bool = True, **kwargs):
 if __name__=='__main__':
     from rfa_toolbox import create_graph_from_pytorch_model, visualize_architecture, input_resolution_range
     import torch
-    model = better_mobilenetV1() # replace with any torch module-object-returning function
+    model = efficient_mobilenetV1() # replace with any torch module-object-returning function
     graph = create_graph_from_pytorch_model(model, input_res=(1, 3, 224, 224))
     flops = FlopCountAnalysis(model, torch.ones(1, 3, 224, 224)).total()
     imin, _ = input_resolution_range(graph)
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('Imin:', imin, '\tGFLOPS:', flops / 1000000000, "MParams:", n_parameters / 1000000)
+    visualize_architecture(graph, "emobilenetV1").view()
